aeddaff89197789b02ba1a501d99f7df0df1f43e
unknown <azizishabbir355@gmail.com>
2026-08-03 15:53:47 +0500

    new project based on html css and simple website intraducing product

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuOpenButton = document.querySelector
+("#menu-open-button");
+
+menuOpenButton.addEventListener("click", () => {
+    // Toggle mobile menu visibility
+    document.body.classList.toggle("show-mobile-menu");
+
+
+});
